c28440936b20ca9c67086fdf1b5cf14afe9165a7
Iain Barnett <iainspeed@gmail.com>
2010-10-08 01:58:31 +0100

    rewrote this so it works, though I had the answer ages ago... can't remember how though! :)

--- 32euler.rb
+++ 32euler.rb
@@ -3,10 +3,13 @@
 #add fold to the available method names
 module Enumerable
   alias :fold inject unless Enumerable.method_defined? :fold
+  def sum
+    self.fold(:+)
+  end
 end
 
 class Fixnum
-  def to_a
+  def to_a # convert an integer to an array of integers 1234 => [1,2,3,4]
     exp = 1
     n = self.divmod(10**exp) #start at 10
     case n.first
@@ -18,49 +21,31 @@ end
 
 ns = (1..9).to_a
 
-#lhs  combination of 9C5 == 126
-#rhs  permutation 4 - 24 possible numbers, 1 / 24 chance lhs == rhs
-
-f = lambda { |xs| xs.fold(0){|sum,x| sum += x * 10**(xs.length - xs.index(x) - 1) }}
 class Array 
-  def sum
-    self.fold(:+)
+  alias :perm permutation #for brevity
+  alias :combi combination
+  def to_i # convert an array of integers to an integer, [1,2,3,4] => 1234
+    self.fold(0){|sum,x| sum += x * 10**(self.length - self.index(x) - 1) }
   end
 end
 
 t1 = Time.now
 total = [ ]
-ks = [[1,4],[2,3]]
-ks.each do |k|
-  ns.combination(k.sum) do |c|  # possible multiplier/multiplicand numbers
-    # 2 digit * 3 digit number - both are permutations
-    m1s = c.combination(k.first) # multiplicand combinations [[1, 2], ...]
-    leftovers = ns - c # 4 digits no used, [6,7,8,9]
-    rhs = leftovers.permutation( 9 - k.sum ) #rhs permutations from remaining numbers
-    # puts "#{rhs.to_a}"
-    m1s.each do |m1| # [1,2] ...
-      m2s = (c - m1).permutation # multiplier permutations [[3, 4, 5], [3, 5, 4], ... ]
-      m1.permutation(m1.length).each do |m1p| #  => [1,2], [2,1] ...
-        m2s.each do |m2| # m2 => [3,4,5], [4,3,5]...
-          p = f.call(m1p) * f.call(m2)
-          next unless p.divmod(10**4).first.zero?
-          if leftovers.all? { |x| p.to_a.include? x } # see if it's worth running test below
-            rhs.each do |rh| #test each product against remaining 4 numbers
-              r = f.call(rh)
-              if r == p
-                unless total.include? p
-                  # puts "> #{f.call(m1p)} * #{f.call(m2)} = #{p}"
-                  total << p 
-                  # puts "total so far: #{total.sum}"
-                end
-              end
-            end #rhs.each
-          end #if
-        end #m2s.each
+(1..2).each do |k| #because the multiplicand will either be 1 or 2 digits
+  ns.combi(5) do |lhs|  # split 9 digits, left hand side is set 
+    rhs = ns - lhs # set the right hand side possible digits
+    lhs.combi(k).each do |cand|
+      cand.perm.map {|cand_p| (lhs - cand).perm.map {|plier| [cand_p,plier] } }.flatten(1).each do |cnd,pli| # same as list comp, only more verbose :(
+        ( p = cnd.to_i * pli.to_i).divmod(10**4).first.zero? || next #skip if not 4 digits, p is final product
+        if rhs.all? { |x| p.to_a.include? x } # break p into array form to see if rhs has all numbers needed
+            rhs.perm( 9 - 5 ) do |rh| 
+              total << p if rh.to_i == p unless total.include? p #no duplicates!
+            end
+        end #if
       end #m1.permutation
-    end #m1s.each
+    end #lhs.combi
   end #ns.combinations
-end
+end # |cnd, pli|
 
 puts "total:  #{total.sum}"
 puts "Time: #{Time.now - t1}"
